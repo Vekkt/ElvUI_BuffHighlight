8df3bbca66c4c95497fd3179798af6824c1b13b4
vekkt <francois.burguet@epfl.ch>
2019-11-05 02:05:03 +0100

    name changed, option added

--- ElvUI_PluginFramework.lua
+++ ElvUI_PluginFramework.lua
@@ -1,16 +1,11 @@
 --[[
 	TODO
-		- Change plugin name
-		- Complete Option panel (enable/disable fading, fading color, priority over debuff, ...)
+		- Complete Option panel (layout, priority over debuff, ...)
 		- Add raid 40 compatibility
-
-
-	KNOWN BUGS
-	       - Highlighting broken when reloging
 ]]
 
 local E, L, V, P, G = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
-local MyPlugin = E:NewModule('MyPluginName', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0'); --Create a plugin within ElvUI and adopt AceHook-3.0, AceEvent-3.0 and AceTimer-3.0. We can make use of these later.
+local GH = E:NewModule('GlimmerHighlight', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0'); --Create a plugin within ElvUI and adopt AceHook-3.0, AceEvent-3.0 and AceTimer-3.0. We can make use of these later.
 local EP = LibStub("LibElvUIPlugin-1.0") --We can use this to automatically insert our GUI tables when ElvUI_Config is loaded.
 local addonName, addonTable = ... --See http://www.wowinterface.com/forums/showthread.php?t=51502&p=304704&postcount=2
 
@@ -18,8 +13,6 @@ local UF = E:GetModule('UnitFrames')
 
 local eps = 0.001
 local glimmerID = 287280
-local glimmerColor = {r = 0.1, g = 0.6, b = 0.3, a = 1.0}
-local glimmerFadeColor = {r = 0.0, g = 0.4, b = 0.1, a = 1.0}
 
 
 local function CheckGlimmer(unit)
@@ -39,18 +32,18 @@ end
 local function DebuffHighlighted(object)
 	local r, g, b, _ = object.DebuffHighlight:GetVertexColor()
 	if r == 0 and g == 0 and b == 0 then return false end
-	local d1 = math.abs(r - glimmerColor.r) < eps and math.abs(g - glimmerColor.g) < eps and math.abs(b - glimmerColor.b) < eps
-	local d2 = math.abs(r - glimmerFadeColor.r) < eps and math.abs(g - glimmerFadeColor.g) < eps and math.abs(b - glimmerFadeColor.b) < eps
+	local d1 = math.abs(r - E.db.GH.glimmerColor.r) < eps and math.abs(g - E.db.GH.glimmerColor.g) < eps and math.abs(b - E.db.GH.glimmerColor.b) < eps
+	local d2 = math.abs(r - E.db.GH.glimmerFadeColor.r) < eps and math.abs(g - E.db.GH.glimmerFadeColor.g) < eps and math.abs(b - E.db.GH.glimmerFadeColor.b) < eps
 	return not (d1 or d2)
 end
 
 local function GlimmerUpdate(object, unit)
 	if DebuffHighlighted(object) then return nil end
 	local glimmerOn = CheckGlimmer(unit)
-	if glimmerOn and glimmerOn > 5 then		
-		object.DebuffHighlight:SetVertexColor(glimmerColor.r, glimmerColor.g, glimmerColor.b, glimmerColor.a)
+	if glimmerOn and (glimmerOn > 5 or not E.db.GH.fadeEnable) then		
+		object.DebuffHighlight:SetVertexColor(E.db.GH.glimmerColor.r, E.db.GH.glimmerColor.g, E.db.GH.glimmerColor.b, E.db.GH.glimmerColor.a)
 	elseif glimmerOn and glimmerOn <= 5 then
-		object.DebuffHighlight:SetVertexColor(glimmerFadeColor.r, glimmerFadeColor.g, glimmerFadeColor.b, glimmerColor.a)
+		object.DebuffHighlight:SetVertexColor(E.db.GH.glimmerFadeColor.r, E.db.GH.glimmerFadeColor.g, E.db.GH.glimmerFadeColor.b, E.db.GH.glimmerColor.a)
 	else
 		object.DebuffHighlight:SetVertexColor(0, 0, 0, 0)
 	end
@@ -77,6 +70,7 @@ local function UpdateInGroup()
 			if ElvUF_Party.groups[1][i] then
 				local frame = ElvUF_Party.groups[1][i]
 				local unit = frame.unit
+				print(unit)
 				if frame.DebuffHighlight then
 					GlimmerUpdate(frame, unit)
 				end
@@ -86,47 +80,86 @@ local function UpdateInGroup()
 end
 
 local function Update()
-	if not E.db.MyPlugin.enable then return nil end
+	if not E.db.GH.enable then return nil end
 	if IsInRaid() then UpdateInRaid()
 	elseif IsInGroup() then UpdateInGroup() end
 end
 
 
 --Default options
-P["MyPlugin"] = {
+P["GH"] = {
 	["enable"] = true,
+	glimmerColor = {r = 0.1, g = 0.6, b = 0.3, a = 1.0},
+	["fadeEnable"] = true,
+	glimmerFadeColor = {r = 0.0, g = 0.4, b = 0.1, a = 1.0},
 }
 
 
 --This function inserts our GUI table into the ElvUI Config. You can read about AceConfig here: http://www.wowace.com/addons/ace3/pages/ace-config-3-0-options-tables/
-function MyPlugin:InsertOptions()
-	E.Options.args.MyPlugin = {
+function GH:InsertOptions()
+	E.Options.args.GH = {
 		order = 100,
 		type = "group",
-		name = "MyPlugin",
+		name = "|cff00b3ffGlimmerHighlight|r",
 		args = {
 			enable = {
 				order = 1,
 				type = "toggle",
-				name = "MyToggle",
+				name = "Enable",
 				get = function(info)
-					return E.db.MyPlugin.enable
+					return E.db.GH.enable
 				end,
 				set = function(info, value)
-					E.db.MyPlugin.enable = value
-					Update() --We changed a setting, call our Update function
+					E.db.GH.enable = value
+				end,
+			},
+			glimmerColor = {
+				order = 2,
+				type = "color",
+				name = "Glimmer Color",
+				get = function(info)
+					return E.db.GH.glimmerColor.r, E.db.GH.glimmerColor.g, E.db.GH.glimmerColor.b, E.db.GH.glimmerColor.a
+				end,
+				set = function(info, r, g, b, a)
+					E.db.GH.glimmerColor.r = r
+					E.db.GH.glimmerColor.g = g
+					E.db.GH.glimmerColor.b = b
+				end,
+			},
+			fadeEnable = {
+				order = 3,
+				type = "toggle",
+				name = "Enable Fade",
+				get = function(info)
+					return E.db.GH.fadeEnable
+				end,
+				set = function(info, value)
+					E.db.GH.fadeEnable = value
+				end,
+			},
+			fadeColor = {
+				order = 4,
+				type = "color",
+				name = "Fade Color",
+				get = function(info)
+					return E.db.GH.glimmerFadeColor.r, E.db.GH.glimmerFadeColor.g, E.db.GH.glimmerFadeColor.b, E.db.GH.glimmerFadeColor.a
+				end,
+				set = function(info, r, g, b, a)
+					E.db.GH.glimmerFadeColor.r = r
+					E.db.GH.glimmerFadeColor.g = g
+					E.db.GH.glimmerFadeColor.b = b
 				end,
 			},
 		},
 	}
 end
 
-function MyPlugin:Initialize()
+function GH:Initialize()
 	--Register plugin so options are properly inserted when config is loaded
-	EP:RegisterPlugin(addonName, MyPlugin.InsertOptions)
+	EP:RegisterPlugin(addonName, GH.InsertOptions)
 end
 
 local f = CreateFrame("Frame")
 f:SetScript("OnUpdate", Update)
 
-E:RegisterModule(MyPlugin:GetName()) --Register the module with ElvUI. ElvUI will now call MyPlugin:Initialize() when ElvUI is ready to load our plugin.
+E:RegisterModule(GH:GetName()) --Register the module with ElvUI. ElvUI will now call GH:Initialize() when ElvUI is ready to load our plugin.
