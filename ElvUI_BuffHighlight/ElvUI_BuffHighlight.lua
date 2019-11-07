63a4e5879a8db59cff34d07f0cd1db314bc8b788
vekkt <francois.burguet@epfl.ch>
2019-11-07 18:14:14 +0100

    added possiblity to track multiple buffs

--- ElvUI_BuffHighlight/ElvUI_BuffHighlight.lua
+++ ElvUI_BuffHighlight/ElvUI_BuffHighlight.lua
@@ -14,15 +14,21 @@ local UF = E:GetModule('UnitFrames')
 
 local eps = 0.002
 
+local function isTracked(spellID)
+	for id, name in pairs(E.db.GH.trackedBuffID) do
+		if name and spellID == tonumber(id) then return true end
+	end
+	return false
+end
+
 local function CheckBuff(unit)
 	if not unit or not UnitCanAssist("player", unit) then return nil end
 	local i = 1
-	local trackedBuffID = E.db.GH.trackedBuffID
 	while true do
 		local _, texture,_,_,_, expire, source,_,_, spellID = UnitAura(unit, i, "HELPFUL")
 		if not texture then break end
 
-		if(spellID == trackedBuffID and source == "player") then
+		if(source == "player" and isTracked(spellID)) then
 			return expire - GetTime()
 		end
 		i = i + 1
@@ -92,7 +98,7 @@ local function UpdateInGroup()
 end
 
 local function Update()
-	if not E.db.GH.enable then return nil end
+	if not E.db.GH or not E.db.GH.enable then return nil end
 	if IsInRaid() then UpdateInRaid()
 	elseif IsInGroup() then UpdateInGroup() end
 end
@@ -112,6 +118,23 @@ local function retrieveIcon(spellName)
 	return icon
 end
 
+local function getBuffList()
+	local str = "\nCurrently tracked buffs:\n"
+	if not E.db.GH.trackedBuffID then return str end
+	for id, name in pairs(E.db.GH.trackedBuffID) do
+		print(id, name)
+		if name then 
+			str = str..string.format("    - %s (%s)\n", name, id)
+		end
+	end
+	return str
+end
+
+local function updateBuffList()
+	E.db.GH.buffList = getBuffList()
+	E.Options.args.GH.args.gr3.args.trackedSpells.name = E.db.GH.buffList
+end
+
 --Default options
 P["GH"] = {
 	["enable"] = true,
@@ -119,7 +142,8 @@ P["GH"] = {
 	["fadeEnable"] = true,
 	["buffFadeColor"] = {r = 0.0, g = 0.4, b = 0.1, a = 1.0},
 	["fadeThreshold"] = 5,
-	["trackedBuffID"] = 287280,
+	["trackedBuffID"] = { ["287280"] = "Glimmer of Light", },
+	["buffList"] = "\nCurrently tracked buffs:\n    - Glimmer of Light (287280)\n",
 }
 
 --This function inserts our GUI table into the ElvUI Config. You can read about AceConfig here: http://www.wowace.com/addons/ace3/pages/ace-config-3-0-options-tables/
@@ -227,21 +251,41 @@ function GH:InsertOptions()
 				name = "Tracked spell",
 				guiInline = true,
 				args = {
-					changeSpell = {
+					addSpell = {
 						order = 10,
 						type = "input",
-						name = "Buff ID",
-						width = 100,
+						name = "Add Buff ID",
+						width = 10,
 						get = function(info)
-							local name = nameFromID(E.db.GH.trackedBuffID)
-							local id = retrieveID(E.db.GH.trackedBuffID)
-							return string.format("%s (%s)", name, id)
+							return "Buff ID"
 						end,
 						set = function(info, data)
-							newID = retrieveID(data)
-							if newID then E.db.GH.trackedBuffID = newID end
+							local newID = retrieveID(data)
+							local name = nameFromID(newID)
+							if newID and name then 
+								E.db.GH.trackedBuffID[tostring(newID)] = name
+							end
+							updateBuffList()
 						end,
 					},
+					delSpell = {
+						order = 12,
+						type = "input",
+						name = "Remove Buff ID",
+						width = 10,
+						get = function(info)
+							return "Buff ID"
+						end,
+						set = function(info, data)
+							E.db.GH.trackedBuffID[tostring(data)] = nil
+							updateBuffList()
+						end,
+					},
+					trackedSpells = {
+						order = 14,
+						type = "description",
+						name = E.db.GH.buffList,
+					}
 				},
 			}
 		},
