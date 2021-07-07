fce5da7701b9db9ebbbeb575bfc8ffd6e8c8cfba
Vekkt <francois.burguet@epfl.ch>
2021-07-07 14:58:18 +0200

    Update profile.lua

--- ElvUI_BuffHighlight/profile.lua
+++ ElvUI_BuffHighlight/profile.lua
@@ -3,6 +3,7 @@ local E, L, V, P, G = unpack(ElvUI);
 P["BH"] = {
 	["enable"] = true,
 	["colorBackdrop"] = false,
+	["fadeEnabled"] = true,
 	["spells"] = {},
 	["refreshRate"] = 0.1,
 	["trackedHeaders"] = {
